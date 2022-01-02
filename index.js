21093fff9a0a5749a693913cbdd686cc0b5b31ad
adithv08 <adithv08@gmail.com>
2022-01-01 20:38:33 -0500

    more edits

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow } = require('electron')
 const { WebSocket } = require('ws');
 
-const ws = new WebSocket('ws://www.host.com/path');
+const ws = new WebSocket('ws://localhost:8080');
 
 ws.on('open', function open() {
     ws.send('something');
