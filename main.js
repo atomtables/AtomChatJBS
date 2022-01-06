7e894528ab670b48b762da853caeb0148b4baa30
adithv08 <adithv08@gmail.com>
2022-01-05 21:47:49 -0500

    bug fix

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ const server = http.createServer(app);
 app.get('/', (req, res) => {
   res.sendFile(__dirname + 'public/index.html');
 });
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
