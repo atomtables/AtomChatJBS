110c3e203669cbe94a3e92b9ef2c8df2a11a1b79
adithv08 <adithv08@gmail.com>
2022-01-05 21:46:42 -0500

    new code

--- main.js
+++ main.js
@@ -4,9 +4,5 @@ const http = require('http');
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+  res.sendFile(__dirname + 'public/index.html');
 });
