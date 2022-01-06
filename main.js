553b2e5d4a28d62d5eda48ba94274420543e5e4c
adithv08 <adithv08@gmail.com>
2022-01-05 21:53:16 -0500

    next stage

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 server.listen(3000, () => {
