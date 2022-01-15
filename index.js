9bd672b5cebf51935eebe10f44e59da5e929994b
adithv08 <adithv08@gmail.com>
2022-01-15 13:27:10 -0500

    going back to vscode + attempting a dark mode

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+app.get('/darkmode', (req, res) => {
+    res.sendFile(__dirname + '/darkmode.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
