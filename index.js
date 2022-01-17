54e1cfa87dcc2ef8c18304ea80cca6c8100f2ca4
adithv08 <adithv08@gmail.com>
2022-01-16 20:47:31 -0500

    notifications + logging added

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const fs = require('fs')
+
+const d = new Date();
+d.setTime(d.getTime())
+let options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+
+fs.writeFile(d.toString("MM_DD_YYYY__HH__MM") + "log-messages.txt", "log-init\n", err => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    //file written successfully
+});
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -20,6 +34,14 @@ app.get('/worker.js', (req, res) => {
 io.on('connection', (socket) => {
     socket.on('chat message', msg => {
         io.emit('chat message', msg);
+        console.log(msg[0])
+        fs.appendFile(d.toString("MM_DD_YYYY__HH__MM") + "log-messages.txt", msg[0] + "\n", err => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            //file written successfully
+        })
     });
 });
 
