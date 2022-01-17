ac9f76c8cb5b908bdebbd1d05f1d5d2366f81c1b
adithv08 <adithv08@gmail.com>
2022-01-17 14:05:55 -0500

    testing out postgresql

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 const fs = require('fs')
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 
 const d = new Date();
 d.setTime(d.getTime())
@@ -16,6 +24,18 @@ fs.writeFile(d.toString("MM_DD_YYYY__HH__MM") + "log-messages.txt", "log-init\n"
     //file written successfully
 });
 
+app.get('/db', async(req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+})
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
