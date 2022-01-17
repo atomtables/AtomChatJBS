8b47673f818e9d7a12604f576993c81ff9fa8b93
adithv08 <adithv08@gmail.com>
2022-01-17 14:29:22 -0500

    morechanges

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ app.get('/db', async(req, res) => {
         const client = await pool.connect();
         const result = await client.query('SELECT * FROM test_table');
         const results = { 'results': (result) ? result.rows : null };
-        res.render('pages/db', results);
+        res.render('pages/json', results);
         client.release();
     } catch (err) {
         console.error(err);
-        res.send("Error " + err);
+        res.send("Error: " + err);
     }
 })
 
