18127c7c113d2bf2ee819b3a1df58d479315f425
atomtables <76572470+atomtables@users.noreply.github.com>
2023-11-30 18:15:00 -0500

    Coming back to this project after almost a year, adding colors to each user's texts, adding "user went online/offline" responses to it, changing repository names and author names from adithv08 to atomtables. this project will be getting more attention, as it will probably be the base for the real AtomChat.

--- index.js
+++ index.js
@@ -4,20 +4,8 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 const fs = require('fs')
 
-
-/*
-const d = new Date();
-d.setTime(d.getTime())
-let options = { year: 'numeric', month: 'numeric', day: 'numeric' };
-// attempt to log messages fail
-
-fs.writeFile(d.toString("MM_DD_YYYY__HH__MM") + "log-messages.txt", "log-init\n", err => {
-    if (err) {
-        console.error(err)
-        return
-    }
-    //file written successfully
-});*/
+var users = []
+var notOnline = []
 
 //mainpage
 app.get('/', (req, res) => {
@@ -30,18 +18,62 @@ app.get('/worker.js', (req, res) => {
     res.sendFile(__dirname + "/public/worker.js")
 })
 
+setInterval(() => {
+    notOnline = JSON.parse(JSON.stringify(users));
+    io.emit("online_check")
+    setTimeout(() => {
+        notOnline.forEach(user => {
+            try {
+                console.log("user went offline: " + user)
+                users.splice(users.indexOf(user), 1)
+            } catch {
+                console.log("error: user not found")
+            }
+        })
+    }, 5000)
+}, 10000)
+
 // message receiver
 io.on('connection', (socket) => {
+    socket.on("is_still_online", username => {
+        if (users.indexOf(username) !== -1) {
+            socket.emit("is_still_online", true)
+        } else {
+            socket.emit("is_still_online", false)
+        }
+    })
+    socket.on("joined", username => {
+        console.log("new user joined: " + username)
+        if (notOnline.indexOf(username) === -1) {
+            users.push(username)
+        }
+        try {
+            notOnline.splice(notOnline.indexOf(username), 1)
+        } catch {
+            console.log("error: user not found")
+        }
+        io.emit("joined", username)
+    })
+    socket.on("left", username => {
+        console.log("user left: " + username)
+        users.splice(users.indexOf(username), 1)
+        io.emit("left", username)
+    })
+    socket.on("online_response", username => {
+        try {
+            notOnline.splice(notOnline.indexOf(username), 1)
+        } catch {
+            console.log("error: user not found")
+        }
+    })
+    socket.on("getUsers", (username) => {
+        var f = JSON.parse(JSON.stringify(users));
+        f.splice(f.indexOf(username), 1)
+        io.emit("getUsers", f)
+    })
     socket.on('chat message', msg => {
-        io.emit('chat message', msg);
-        console.log(msg[0])
-        fs.appendFile(d.toString("MM_DD_YYYY__HH__MM") + "log-messages.txt", msg[0] + "\n", err => {
-            if (err) {
-                console.error(err)
-                return
-            }
-            //file written successfully
-        })
+        io.emit("chat message", msg)
+        console.log(msg[0], msg[1], msg[2])
     });
 });
 // http server init
